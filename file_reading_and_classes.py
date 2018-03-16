d1d5c15397b48ba55369d5cdb9683e1da939dd36
Rafael Felix Silva Requião <rafaelfrequiao@gmail.com>
2018-03-16 03:16:54 -0300

    entendi classes - implementar no rascunho.py

--- file_reading_and_classes.py
+++ file_reading_and_classes.py
@@ -15,27 +15,39 @@ class Questao:
 	"Uma questao com muitas coisas dentro"
 	questoes_contador = 0
 
-	def __init__(self, numero, resolucao): #simplificacao, grafico, link, raizes)
+	#def __call__(self):
+	#	return self
+
+	def __init__(self, numero, questao, resolucao): #simplificacao, grafico, link, raizes)
 		self.numero = numero
+		self.questao = questao
 		self.resolucao = resolucao
 		Questao.questoes_contador += 1
+		
 	
-	def contarQuestoes(self):
+	def contador(self):
 		print("[DEBUG] Numero de questoes = " + Questao.questoes_contador)	
+		
 
-
-	def mostrarQuestao(self):
-		print("[DEBUG] numero = " + self.numero + ", \nResolucao = " + self.resolucao + " \n")
-
-
-
+	def mostrar(self):
+		print("questao = " + self.questao)
+		print("numero = " + str(self.numero) + ", \nResolucao = " + self.resolucao + " \n")
+		
 #fim de class Questao()
 
+q_list = []
+tratado = []
+
+
+#mock de resolucao
+resolucoes = ["aehooo", "12345", "arco tg minha pica", "nao da pra ler", "typo errado"]
+
+
+# abrir arquivo
 f = open("questoes_utf8.txt", "r", encoding="ascii")
 questoes = f.readlines()
 f.close()
 
-tratado = []
 
 #tratar linhas em branco antes ou depois?
 for i in range(0, len(questoes)):
@@ -46,9 +58,16 @@ for i in range(0, len(questoes)):
 		tratado.append(questoes[i].strip("\n"))	
 
 
-print(questoes)
-print(tratado)
+print("\n[DEBUG] " + str(tratado) + "\n\n")
+
+
+for obj in range(len(tratado)):
+	obj = Questao(obj, tratado[obj], resolucoes[obj])
+	q_list.append(obj)	
+
 
 for i in range(0, len(tratado)):
-		
+	print(q_list[i].mostrar())
 
+
+#EOF
